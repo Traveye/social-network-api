d6f32f46f765490a02b16438ec6e89e490b51023
Big Trees <traveye@protonmail.com>
2023-02-22 15:54:55 -0800

    adding thought model

--- models/thought.js
+++ models/thought.js
@@ -1,35 +1,26 @@
-// Thought:
+const mongoose = require('mongoose');
 
+const thoughtSchema = new mongoose.Schema({
+    thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (createdAtVal) => toLocaleDateString(createdAtVal),
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    reactions: [
+        reactionSchema
+    ],
+});
 
-// thoughtText
-
-// String
-// Required
-// Must be between 1 and 280 characters
-
-
-
-// createdAt
-
-// Date
-// Set default value to the current timestamp
-// Use a getter method to format the timestamp on query
-
-
-
-// username (The user that created this thought)
-
-// String
-// Required
-
-
-
-// reactions (These are like replies)
-
-// Array of nested documents created with the reactionSchema
-
-
-
-
-// Schema Settings:
-// Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
+thoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
