f20efb763d5bdfc2f1f83be6b6c3e5996000b061
Big Trees <traveye@protonmail.com>
2023-02-27 22:56:39 -0800

    asyncing user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,143 +1,109 @@
 // /api/users
 const router = require("express").Router();
 const { User } = require("../models");
-// GET all users
 
-router.get("/", (req, res) => {
-  User.find({}, (err, result) => {
-    if (err) {
-      console.log("Error: ", err);
-      res.status(500).json(err);
-    } else {
-      res.json(result);
-    }
-  });
+// GET all users
+router.get("/", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.json(users);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server error' });
+  }
 });
 
 // GET a single user by its _id and populated thought and friend data
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id)
+      .populate("thoughts")
+      .populate("friends");
 
-router.get("/:id", (req, res) => {
-  User.findById(req.params.id)
-    .populate("thoughts")
-    .populate("friends")
-    .exec((err, result) => {
-      if (err) {
-        console.log("Error: ", err);
-        res.status(500).json(err);
-      } else {
-        res.json(result);
-      }
-    });
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.json(user);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server error' });
+  }
 });
 
-// POST a new user:
-
-// // example data
-// {
-//   "username": "lernantino",
-//   "email": "lernantino@gmail.com"
-// }
-
-router.post("/", (req, res) => {
-  User.create(req.body, (err, result) => {
-    if (err) {
-      console.log("Error: ", err);
-      res.status(500).json(err);
-    } else {
-      res.json(result);
-    }
-  });
+// POST a new user
+router.post("/", async (req, res) => {
+  try {
+    const newUser = await User.create(req.body);
+    res.json(newUser);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server error' });
+  }
 });
 
 // PUT to update a user by its _id
-
-router.put("/:id", (req, res) => {
-  User.updateOne({ _id: req.params.id }, { $set: req.body }, (err, result) => {
-    if (err) {
-      console.log("Error: ", err);
-      res.status(500).json(err);
-    } else {
-      res.json(result);
-    }
-  });
+router.put("/:id", async (req, res) => {
+  try {
+    const updatedUser = await User.updateOne({ _id: req.params.id }, { $set: req.body });
+    res.json(updatedUser);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server error' });
+  }
 });
 
-
 // DELETE to remove user by its _id
-
-router.delete("/:id", (req, res) => {
-    User.deleteOne({ _id: req.params.id }, (err, result) => {
-        if (err) {
-        console.log("Error: ", err);
-        res.status(500).json(err);
-        } else {
-        res.json(result);
-        }
-    });
+router.delete("/:id", async (req, res) => {
+  try {
+    const deletedUser = await User.deleteOne({ _id: req.params.id });
+    res.json(deletedUser);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server error' });
+  }
 });
 
 // BONUS: Remove a user's associated thoughts when deleted.
-
-router.delete("/:id", (req, res) => {
-    User.deleteOne({ _id: req.params.id }, (err, result) => {
-        if (err) {
-        console.log("Error: ", err);
-        res.status(500).json(err);
-        } else {
-        res.json(result);
-        }
-    });
+router.delete("/:id", async (req, res) => {
+  try {
+    const deletedUser = await User.deleteOne({ _id: req.params.id });
+    await Thought.deleteMany({ username: deletedUser.username });
+    res.json(deletedUser);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server error' });
+  }
 });
 
 // /api/users/:userId/friends/:friendId
 
-router.post("/:userId/friends/:friendId", (req, res) => {
-    User.updateOne(
-        { _id: req.params.userId },
-        { $push: { friends: req.params.friendId } },
-        (err, result) => {
-        if (err) {
-            console.log("Error: ", err);
-            res.status(500).json(err);
-        } else {
-            res.json(result);
-        }
-        }
-    );
-});
-
 // POST to add a new friend to a user's friend list
-
-router.post("/:userId/friends/:friendId", (req, res) => {
-    User.updateOne(
-        { _id: req.params.userId },
-        { $push: { friends: req.params.friendId } },
-        (err, result) => {
-        if (err) {
-            console.log("Error: ", err);
-            res.status(500).json(err);
-        } else {
-            res.json(result);
-        }
-        }
+router.post("/:userId/friends/:friendId", async (req, res) => {
+  try {
+    const updatedUser = await User.updateOne(
+      { _id: req.params.userId },
+      { $push: { friends: req.params.friendId } }
     );
+    res.json(updatedUser);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server error' });
+  }
 });
 
 // DELETE to remove a friend from a user's friend list
-
-router.delete("/:userId/friends/:friendId", (req, res) => {
-    User.updateOne(
-        { _id: req.params.userId },
-        { $pull: { friends: req.params.friendId } },
-        (err, result) => {
-        if (err) {
-            console.log("Error: ", err);
-            res.status(500).json(err);
-        } else {
-            res.json(result);
-        }
-        }
+router.delete("/:userId/friends/:friendId", async (req, res) => {
+  try {
+    const updatedUser = await User.updateOne(
+      { _id: req.params.userId },
+      { $pull: { friends: req.params.friendId } }
     );
+    res.json(updatedUser);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Server error' });
+  }
 });
 
 module.exports = router;
